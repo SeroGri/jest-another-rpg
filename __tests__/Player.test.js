e7568dfa25072d2a7e861f1049ff744333a82c5a
SeroGri <sero.grigoryan@gmail.com>
2021-12-23 03:38:50 -0800

    edit

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,33 +1,34 @@
-const Potion = require('../lib/Potion');
+const Player = require('../lib/Player.js');
+const Potion = require('../lib/Potion.js');
 
-jest.mock('../lib/Potion');
+jest.mock('../lib/Potion.js');
 
-const Player = require('../lib/Player');
+test('creates a player object', () => {
+  const player = new Player('Dave');
 
-function Player(name = '') {
-    this.name = name;
-  
-    this.health = Math.floor(Math.random() * 10 + 95);
-    this.strength = Math.floor(Math.random() * 5 + 7);
-    this.agility = Math.floor(Math.random() * 5 + 7);
-  
-    this.inventory = [new Potion('health'), new Potion()];
-  
-    // returns an object with various player properties
-    this.getStats = function() {
-      return {
-        potions: this.inventory.length,
-        health: this.health,
-        strength: this.strength,
-        agility: this.agility
-      };
-    };
-  
-    // returns the inventory array or false if empty
-    this.getInventory = function() {
-      if (this.inventory.length) {
-        return this.inventory;
-      }
-      return false;
-    };
-  }
+  expect(player.name).toBe('Dave');
+  expect(player.health).toEqual(expect.any(Number));
+  expect(player.strength).toEqual(expect.any(Number));
+  expect(player.agility).toEqual(expect.any(Number));
+
+  expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
+
+test("gets player's stats as an object", () => {
+  const player = new Player('Dave');
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+  const player = new Player('Dave');
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
+});
